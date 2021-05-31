573d1cb20f03b48a6c47180cd3183ca3e8e4c35f
johnjeang <johnjeang@gmail.com>
2021-05-31 16:16:43 -0500

    Initial Commit

---
+++ iExpress/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpress
+//
+//  Created by John Jeang on 5/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpress/iExpressApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpressApp.swift
+//  iExpress
+//
+//  Created by John Jeang on 5/31/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
